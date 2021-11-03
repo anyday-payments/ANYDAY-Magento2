8331bc6359b71dab38402acd8a5eeb1526ea79f3
Mayur Kathale <mayur@morphos.is>
2021-11-03 08:19:27 +0530

    Adding pricetag under the payment type

--- view/frontend/web/js/view/payment/method-renderer/adpayment.js
+++ view/frontend/web/js/view/payment/method-renderer/adpayment.js
@@ -1,9 +1,10 @@
 define(
     [
         'Magento_Checkout/js/view/payment/default',
-        'Anyday_PaymentAndTrack/js/model/redirect-afterorder'
+        'Anyday_PaymentAndTrack/js/model/redirect-afterorder',
+        'Magento_Checkout/js/model/quote'
     ],
-    function (Component, redirectUrlAny) {
+    function (Component, redirectUrlAny, quote) {
         'use strict';
 
         let selfThis = this;
@@ -30,6 +31,64 @@ define(
                 this.redirectAfterPlaceOrder = false;
                 getUrlAnyday();
                 // Override this function and put after place order logic here
+            },
+
+            /**
+             * @return {*}
+             */
+             isDisplayed: function () {
+                if (window.anydaytag.is_enable) {
+                    return true;
+                }
+                return false;
+            },
+
+            /**
+             * @return {*|String}
+             */
+            tagToken: function () {
+                return window.anydaytag.tag_code;
+            },
+
+            /**
+             * @return {*|String}
+             */
+            getCurrencyCode:function () {
+                return window.anydaytag.currency_code;
+            },
+
+            /**
+             * @return {*}
+             */
+            isDisplayed: function () {
+                if (window.anydaytag.is_enable) {
+                    return true;
+                }
+                return false;
+            },
+
+            /**
+             * Get pure value.
+             */
+            getPureValue: function () {
+                var totals = quote.getTotals()();
+
+                if (totals) {
+                    return totals['base_grand_total'];
+                }
+
+                return quote['grand_total'];
+            },
+
+            /**
+             * @return {*|String}
+             */
+            getValue: function () {
+                return this.getPureValue();
+            },
+
+            moveElement: function () {
+                eval(window.anydaytag.inline_css);
             },
 
             getLogoUrl: function () {
