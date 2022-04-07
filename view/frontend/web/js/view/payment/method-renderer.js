45c9707db852322a7c3c12fc792af4576dbf9b75
Mayur Kathale <mayur.kathale@gmail.com>
2022-04-07 14:25:47 +0700

    Fixing order total calculation issue

--- view/frontend/web/js/view/payment/method-renderer.js
+++ view/frontend/web/js/view/payment/method-renderer.js
@@ -1,17 +1,14 @@
 define([
     'uiComponent',
     'Magento_Checkout/js/model/payment/renderer-list',
-    'Magento_Checkout/js/model/totals'
-], function (Component, rendererList, totals) {
+], function (Component, rendererList) {
     'use strict';
-        if (parseFloat(totals.getSegment('grand_total').value) >= 300) {
-            rendererList.push(
-                {
-                    type: 'anyday',
-                    component: 'Anyday_Payment/js/view/payment/method-renderer/adpayment'
-                }
-            );
-        }
+        rendererList.push(
+            {
+                type: 'anyday',
+                component: 'Anyday_Payment/js/view/payment/method-renderer/adpayment'
+            }
+        );
         return Component.extend({});
     }
 );
