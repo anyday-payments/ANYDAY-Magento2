90e79f12116e3c4c7ee64e0b390fe7428191cecf
Mayur Kathale <mayur.kathale@gmail.com>
2022-04-26 15:47:38 +0700

    Removing parent transaction ID from capture transaction

--- Service/Anyday/Transaction.php
+++ Service/Anyday/Transaction.php
@@ -75,6 +75,7 @@ class Transaction
                 if ($typeTxn == TransactionInterface::TYPE_AUTH) {
                     $transaction->setIsClosed(false);
                 }
+                $transaction->setParentTxnId(null);
                 $this->repositoryTransaction->save($transaction);
             }
         }
